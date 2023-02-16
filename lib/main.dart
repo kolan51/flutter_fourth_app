0612ca140051f60f9eb4a6f96a793cda34d8ba69
Jasa <jasa1244@gmail.com>
2023-02-16 11:36:24 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,52 @@ import 'package:provider/provider.dart';
 
 import '../screens/products_overview_screen.dart';
 import '../screens/product_detail_screen.dart';
+import '../screens/cart_screen.dart';
+import '../screens/orders_screen.dart';
+import '../screens/user_products_screen.dart';
+import '../screens/edit_product_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Orders(),
+        )
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'MyShop',
+        title: 'My Shop',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-              .copyWith(secondary: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+            errorColor: Colors.red,
+          ).copyWith(secondary: Colors.deepOrange),
           fontFamily: 'Lato',
+          textTheme: TextTheme(
+            titleLarge: TextStyle(color: Colors.purple),
+          ),
         ),
         home: ProductsOverviewScreen(),
-        routes: {ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()},
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
+        },
       ),
     );
   }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/product_item.dart';
 import '../providers/products.dart';
+import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
-
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, idx) => ChangeNotifierProvider.value(
-        value: products[idx],
-        child: ProductItem(
-            // products[idx].id,
-            // products[idx].title,
-            // products[idx].imageUrl,
-            ),
-      ),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+            // builder: (c) => products[i],
+            value: products[i],
+            child: ProductItem(
+                // products[i].id,
+                // products[i].title,
+                // products[i].imageUrl,
+                ),
+          ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
